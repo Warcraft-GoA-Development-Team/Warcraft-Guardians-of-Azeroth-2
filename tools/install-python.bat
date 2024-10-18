89515a1fbf88518f141003e510df1c07cb0ea147
Robmart <robmart@users.noreply.github.com>
2024-10-19 00:16:20 +0200

    Fix BOM again...

--- tools/install-python.bat
+++ tools/install-python.bat
@@ -1,3 +1,3 @@
-﻿powershell.exe -command "Invoke-WebRequest  -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe' -OutFile './python-amd64.exe'"
+powershell.exe -command "Invoke-WebRequest  -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe' -OutFile './python-amd64.exe'"
 .\python-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del "./python-amd64.exe"
